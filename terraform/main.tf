b30eef368f0e18ef1a668a6fb012cbcc55383347
Kevin James <KevinJames@thekev.in>
2016-07-22 22:34:03 -0400

    clean up alpha module

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,12 +1,21 @@
-module "alpha-demo" {
-  source = "./modules/alpha-demo"
+provider "aws" {
+  access_key = "${var.aws_access_key}"
+  secret_key = "${var.aws_secret_key}"
 
-  aws_access_key = "${var.aws_access_key}"
-  aws_secret_key = "${var.aws_secret_key}"
+  region = "${var.region}"
+}
+
+
+module "common" {
+  source = "./modules/common"
 
   key_name = "${var.key_name}"
   key_path = "${var.key_path}"
+}
 
-  region = "${var.region}"
+module "alpha-demo" {
+  source = "./modules/alpha-demo"
+
+  key_name = "${var.key_name}"
   ami = "${lookup(var.amis, var.region)}"
 }
--- terraform/modules/alpha-demo/variables.tf
+++ terraform/modules/alpha-demo/variables.tf
@@ -1,10 +1,4 @@
-variable "aws_access_key" {}
-variable "aws_secret_key" {}
-
 variable "key_name" {}
-variable "key_path" {}
-
-variable "region" {}
 
 variable "ami" {}
 variable "instance_type" {
