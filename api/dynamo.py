9774db3fcc2ad6195bccd02bddc174878a3384be
Kyle Lexmond <github@kyl191.net>
2016-10-30 13:07:40 -0700

    address Kevin's comments

--- api/dynamo.py
+++ api/dynamo.py
@@ -11,75 +11,73 @@ dynamo = dynamo_boto.resource('dynamodb', region_name=DYNAMO_REGION)\
     .Table(DYNAMO_TABLE)
 
 
-def inspect_return(r):
-    status = r.get("ResponseMetadata", {}).get("HTTPStatusCode", 500)
+def inspect_return(response):
+    status = response.get("ResponseMetadata", {}).get("HTTPStatusCode", 500)
     if not flask_api.status.is_success(status):
         log.error("Dynamo return != 2xx; wasn't exception: %d", status)
-        log.error(r)
-        r = {"errors": [{"title": "Internal Error", "detail": status}]}
-    return r, status
+        log.error(response)
+        response = {"errors": [{"title": "Internal Error", "detail": status}]}
+    return response, status
 
 
 def inspect_error(e):
     log.error(e)
-    if not hasattr(e, "response") or e.response.get("Error") is None:
+    if not hasattr(e, "response") or not e.response.get("Error"):
         return {"errors": [{"title": "Internal Error"}]}, \
             flask_api.status.HTTP_500_INTERNAL_SERVER_ERROR
-    elif e.response.get("Error").get("Code") == \
+    if e.response.get("Error").get("Code") == \
             "ConditionalCheckFailedException":
         return {"data": "No changes"}, \
             flask_api.status.HTTP_304_NOT_MODIFIED
-    else:
-        return {"errors": [{
-            "title": e.response.get("Error", {}).get("Code"),
-            "detail": e.response.get("Error", {}).get("Message")
-            }]}, \
-            flask_api.status.HTTP_500_INTERNAL_SERVER_ERROR
+    return {"errors": [{
+        "title": e.response.get("Error", {}).get("Code"),
+        "detail": e.response.get("Error", {}).get("Message")
+        }]}, \
+        flask_api.status.HTTP_500_INTERNAL_SERVER_ERROR
 
 
 def get(params):
     try:
-        r = dynamo.query(**params)
-        return inspect_return(r)
+        response = dynamo.query(**params)
+        return inspect_return(response)
     except Exception as e:
         return inspect_error(e)
 
 
 def get_single(params):
-    r, status = get(params)
+    response, status = get(params)
 
     if not flask_api.status.is_success(status):
-        return r, status
+        return response, status
 
-    if r.get("Count", 0) != 1:
+    if response.get("Count", 0) != 1:
         return {"errors": [
             {"title": "The post you are trying to access doesn't exist."}]},\
             flask_api.status.HTTP_404_NOT_FOUND
 
-    return r.get("Items"), status
+    return response.get("Items"), status
 
 
 def patch(params):
     try:
-        r = dynamo.update_item(**params)
-        return inspect_return(r)
+        return inspect_return(dynamo.update_item(**params))
     except Exception as e:
-        # FIXME: fine grained exception here: ConditionalCheckFailedException
+        # FIXME: move fine grained exception here:
+        # Need to find type of ConditionalCheckFailedException first
         return inspect_error(e)
 
 
 def put(params):
     try:
-        r = dynamo.put_item(**params)
-        return inspect_return(r)
+        return inspect_return(dynamo.put_item(**params))
     except Exception as e:
         return inspect_error(e)
 
 
 def delete(params):
     try:
-        r = dynamo.delete_item(**params)
-        r, status = inspect_return(r)
+        response = dynamo.delete_item(**params)
+        response, status = inspect_return(response)
         if status == 200:
             return None, flask_api.status.HTTP_204_NO_CONTENT
     except Exception as e:
