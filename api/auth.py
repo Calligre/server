0c75d66340eef1c4bc81a4b9c8eaba6b3caad8f6
Kevin James <KevinJames@thekev.in>
2016-11-05 19:35:35 -0700

    extract token decode into new function

--- api/auth.py
+++ api/auth.py
@@ -16,7 +16,37 @@ log = logging.getLogger()
 log.setLevel(logging.INFO)
 
 
-def auth_enabled():
+def decode(token):
+    try:
+        secret = base64.b64decode(
+            AUTH0_SECRET_ID.replace('_', '/').replace('-', '+'))
+        payload = jwt.decode(token, secret, audience=AUTH0_CLIENT_ID)
+        return payload, flask_api.status.HTTP_200_OK
+    except jwt.ExpiredSignature:
+        data = {'errors': [{
+            'title': 'authorization error',
+            'detail': 'expired token'}]}
+        return data, flask_api.status.HTTP_401_UNAUTHORIZED
+    except jwt.InvalidAudienceError:
+        data = {'errors': [{
+            'title': 'authorization error',
+            'detail': 'incorrect audience'}]}
+        return data, flask_api.status.HTTP_401_UNAUTHORIZED
+    except jwt.DecodeError:
+        data = {'errors': [{
+            'title': 'authorization error',
+            'detail': 'invalid signature'}]}
+        return data, flask_api.status.HTTP_401_UNAUTHORIZED
+    except Exception as e:
+        log.exception(e)
+        data = {'errors': [{
+            'title': 'authorization error',
+            'detail': 'unknown error occured while parsing authentication',
+            'source': {'exception': str(e)}}]}
+        return data, flask_api.status.HTTP_401_UNAUTHORIZED
+
+
+def enabled():
     return AUTH0_CLIENT_ID and AUTH0_SECRET_ID
 
 
@@ -24,7 +54,7 @@ def requires_auth(function):
     # pylint: disable=too-many-return-statements
     @functools.wraps(function)
     def decorated(*args, **kwargs):
-        if not auth_enabled():
+        if not enabled():
             _request_ctx_stack.top.current_user = 'test-user'
             return function(*args, **kwargs)
 
@@ -49,31 +79,9 @@ def requires_auth(function):
                 'detail': 'authorization header must start with Bearer'}]}
             return data, flask_api.status.HTTP_401_UNAUTHORIZED
 
-        try:
-            secret = base64.b64decode(
-                AUTH0_SECRET_ID.replace('_', '/').replace('-', '+'))
-            payload = jwt.decode(token, secret, audience=AUTH0_CLIENT_ID)
-        except jwt.ExpiredSignature:
-            data = {'errors': [{
-                'title': 'authorization error',
-                'detail': 'expired token'}]}
-            return data, flask_api.status.HTTP_401_UNAUTHORIZED
-        except jwt.InvalidAudienceError:
-            data = {'errors': [{
-                'title': 'authorization error',
-                'detail': 'incorrect audience'}]}
-            return data, flask_api.status.HTTP_401_UNAUTHORIZED
-        except jwt.DecodeError:
-            data = {'errors': [{
-                'title': 'authorization error',
-                'detail': 'invalid signature'}]}
-            return data, flask_api.status.HTTP_401_UNAUTHORIZED
-        except Exception as e:
-            log.exception(e)
-            data = {'errors': [{
-                'title': 'authorization error',
-                'detail': 'unknown error occured while parsing authentication',
-                'source': {'exception': str(e)}}]}
+        payload, status = decode(token)
+        if not flask_api.status.is_success(status):
+            return payload, status
 
         _request_ctx_stack.top.current_user = payload
         return function(*args, **kwargs)
