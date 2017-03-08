7282be0d28fdaf31314c371de9fc7c4db129f3d1
Kevin James <KevinJames@thekev.in>
2017-03-08 17:54:46 -0500

    fix(stream): return stupid stream id for better distinct

--- api/models/stream.py
+++ api/models/stream.py
@@ -8,4 +8,4 @@ from api.database import gets
 class StreamList(flask_restful.Resource):
     @requires_auth
     def get(self):
-        return gets('stream', 'SELECT DISTINCT id, stream FROM event')
+        return gets('stream', "SELECT DISTINCT 'foo' as id, stream FROM event")
